d3b88dbeb6c9936c48b564643279f0ce8343d623
Daniil Ulyanov <ps1x888@icloud.com>
2024-10-17 21:03:01 +0300

    update

--- arguments.cpp
+++ arguments.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstring>
 
-#include "arguments.h"
+#include "./arguments.h"
 
 void Arguments::CountArgumentSize(
     const char* arg, int &value_size, int &name_size
@@ -65,7 +65,7 @@ void Arguments::WriteArgValue(const char* arg, char* arg_value) {
 }
 
 void Arguments::ParseOneArg(
-    const char* arg, char** arg_name, char** arg_value
+    const char* arg, char*& arg_name, char*& arg_value
 ) {
     int value_size = 0;
     int name_size = 0;
@@ -85,13 +85,13 @@ void Arguments::ParseOneArg(
     }
 
     if (strcmp(name, "") != 0) {
-        *arg_name = name;
+        arg_name = name;
     } else {
         delete [] name;
     }
 
     if (strcmp(value, "") != 0) {
-        *arg_value = value;
+        arg_value = value;
     } else {
         delete [] value;
     }
@@ -118,136 +118,63 @@ bool Arguments::ArgCmp(
 int Arguments::ProcessArg(
     const char* value, const char* follow_arg, const char** endpoint
 ) {
-        if (value) {
-            *(endpoint) = value;
-            return this->kProcessGivenArg;
-        }
-
-        if (!follow_arg) {
-            return this->kProcessArgWithErr;
-        }
-        
-        char* follow_name = nullptr;
-        char* follow_value = nullptr;
-
-        ParseOneArg(follow_arg, &follow_name, &follow_value);
-
-        *(endpoint) = follow_value;
-
-        delete [] follow_name;
-
-        return this->kProcessFollowArg;
+    if (value) {
+        *(endpoint) = value;
+        return EXIT_SUCCESS;
     }
+
+    if (!follow_arg) {
+        return EXIT_FAILURE;
+    }
+    
+    char* follow_name = nullptr;
+    char* follow_value = nullptr;
+
+    ParseOneArg(follow_arg, follow_name, follow_value);
+
+    *(endpoint) = follow_value;
+
+    delete [] follow_name;
+
+    return EXIT_SUCCESS;
+}
 
 int Arguments::ProcessArg(
     const char* value, const char* follow_arg, const int** endpoint
 ) {
     if (value) {
         *(endpoint) = new int {std::atoi(value)};
+        delete [] value;
     } else {
         if (!follow_arg) {
-            return this->kProcessArgWithErr;
+            return EXIT_FAILURE;
         }
         
         char* follow_name = nullptr;
         char* follow_value = nullptr;
 
-        ParseOneArg(follow_arg, &follow_name, &follow_value);
+        ParseOneArg(follow_arg, follow_name, follow_value);
 
         *(endpoint) = new int {std::atoi(follow_value)};
 
         delete [] follow_name;
+        delete [] follow_value;
 
-        return this->kProcessFollowArg;
+        return EXIT_SUCCESS;
     }
 
-    return this->kProcessGivenArg;
+    return EXIT_SUCCESS;
 }
 
 template<typename endp>
-int ParsingResult(endp endpoint, char** argv, int& iter) {
-
-    int result = ProcessArg(argv[iter], argv[iter + 1], endpoint);
-    if (result == this->kProccessArgWithErr) {
-        this->ErrorOnParsingArgument(argv[iter]);
+int Arguments::ParsingResult(
+    endp endpoint, char* arg_value, char** argv, int& iter
+) {
+    int result = ProcessArg(arg_value, argv[++iter], endpoint);
+    if (result == EXIT_FAILURE) {
+        Arguments::ErrorOnParsingArgument(argv[iter]);
         return EXIT_FAILURE;
     }
 
-    iter++;
+    return EXIT_SUCCESS;
 }
-
-// int Arguments::Parse(
-//     int argc, char** argv, 
-//     ComparatorValue* comparator(char* arg_name, char* arg_value)
-// ) {
-//     for (int i = 0; i < argc; ++i) {
-//         char* arg_name = nullptr;
-//         char* arg_value = nullptr;
-
-//         ParseOneArg(argv[i], &arg_name, &arg_value);
-
-//         ComparatorValue* status_code = comparator(arg_name, arg_value);
-//         if (status_code->code == this->kComparatorShortArg)
-//         {
-//             status_code->endpoint = new bool {true};
-//         }
-//         else if (status_codecode == this->kComparatorLongArg)
-//         {
-//             this->ParsingResult(status_code->endpoint, argv, i);
-//         }
-//         else
-//         {
-//             this->ErrorOnParsingArgument(argv[i]);
-//             return EXIT_FAILURE;
-//         }
-
-//         delete status_code;
-//         delete [] arg_name;
-//     }
-
-//     return EXIT_SUCCESS;
-// }
-
-// int ParseArguments(const int argc, char** argv) {
-//     for (int i = 1; i < argc; ++i) 
-//     {
-//         char* arg_name = nullptr;
-//         char* arg_value = nullptr;
-
-//         ParseOneArg(argv[i], &arg_name, &arg_value);
-  
-//         if (ArgCmp(arg_name, "p", "print")) 
-//         {
-//             this->print_request = new bool{true};
-//         } 
-//         else if (ArgCmp(arg_name, "o", "output")) 
-//         {
-//             ParsingResult(&this->path, argv, i);
-//         } 
-//         else if (ArgCmp(arg_name, "s", "stats")) 
-//         {
-//             ParsingResult(&this->stats, argv, i);
-//         } 
-//         else if (ArgCmp(arg_name, "w", "window")) 
-//         {
-//             ParsingResult(&this->window, argv, i);
-//         } 
-//         else if (ArgCmp(arg_name, "f", "from")) 
-//         {
-//             ParsingResult(&this->from_time, argv, i);
-//         } 
-//         else if (ArgCmp(arg_name, "e", "to"))
-//         {
-//             ParsingResult(&this->to_time, argv, i);
-//         } 
-//         else 
-//         {
-//             this->ErrorOnParsingArgument(argv[i]);
-//             return EXIT_FAILURE;
-//         }
-        
-//         delete [] arg_name;
-//     }
-
-//     return EXIT_SUCCESS;
-// }
